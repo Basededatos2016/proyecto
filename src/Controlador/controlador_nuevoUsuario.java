09437a048e25722ecf9f9e59afe5a3f2ef68def9
Sulpick <sbarbella7@gmail.com>
2016-12-11 23:58:40 -0400

    Trabajando controlador ventas - Nuevo usuario

---
+++ src/Controlador/controlador_nuevoUsuario.java
@@ -0,0 +1,7 @@
+package Controlador;
+
+/**
+ * Created by sulpickb on 11/12/16.
+ */
+public class controlador_nuevoUsuario {
+}
--- src/Controlador/controlador_ventas.java
+++ src/Controlador/controlador_ventas.java
@@ -5,28 +5,43 @@ package Controlador;
  */
 
 import java.net.URL;
-        import java.util.ResourceBundle;
-        import javafx.fxml.FXML;
-        import javafx.scene.layout.AnchorPane;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+import conexionDB.conexion_db;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.AnchorPane;
+import Main.Main;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import javafx.scene.control.Label;
 
 
-public class controlador_ventas {
+public class controlador_ventas implements Initializable {
 
-    @FXML
-    private ResourceBundle resources;
+    @FXML    private ResourceBundle resources;
 
-    @FXML
-    private URL location;
+    @FXML     private URL location;
 
-    @FXML
-    private AnchorPane ventana_ventas;
+    @FXML     private AnchorPane ventana_ventas;
 
 
-    @FXML
-    public void initialize() {
-        assert ventana_ventas != null : "fx:id=\"ventana_ventas\" was not injected: check your FXML file 'Ventas.fxml'.";
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
 
 
     }
+
+
 
 }
